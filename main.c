1ce422ee2339955bb0fcc77e00907504c6d2a38f
sb7297 <sbaireddy7297@gmail.com>
2021-03-03 15:05:04 -0500

    read rom file

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
     chip8_init();
+    
+    FILE* fp = fopen("test_opcode.ch8", "rb");
+    uint8_t buf[0xDFF] = {0};
+    fread(buf, sizeof(buf), 1, fp);
 }
